4b8a59f97fe918435047ba444bae2025a05d5d55
shrikha1 <mahanty.shrikha@gmail.com>
2020-10-14 09:10:23 +0530

    negative input, fix bug.

--- sqrt.c
+++ sqrt.c
@@ -27,7 +27,10 @@ int main(int argc, char* argv[]) {
 	if(check_input(argv[1]))
 	{
 		int input = atoi(argv[1]);
-		printf("Sqrt of %d is %f\n",input,sqrt(input));
+		if(input<0)
+			printf("Sqrt of %d is %fi\n",input,sqrt((input*-1)));
+		else
+			printf("Sqrt of %d is %f\n",input,sqrt(input));
 		printf("End of program. Exiting.");
 	}
 	else
